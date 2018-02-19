6105c09388b3db9f56aad2f19db840e94468e137
armandoamador <arm.amador@gmail.com>
2018-02-19 12:11:37 -0500

    Add description of kata in ghost.rb

--- ruby/ghost/ghost.rb
+++ ruby/ghost/ghost.rb
@@ -1,3 +1,7 @@
+#Create a class Ghost
+# Ghost objects are instantiated without any arguments.
+# Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
+
 class Ghost
   attr_reader :color
 
